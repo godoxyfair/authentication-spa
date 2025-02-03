c62f23a64804f50bc84d24afdff93496a57aabd3
m.igumenova <m.igumenova@redmadrobot.com>
2025-02-03 19:52:53 +0100

    gh-pages init

--- src/app/router/Router.tsx
+++ src/app/router/Router.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-// import styles from './stylesStarts.module.scss';
 import { routes } from './routes';
 
 const browserRouter = createBrowserRouter(routes);
@@ -9,21 +8,5 @@ const browserRouter = createBrowserRouter(routes);
  * App routers.
  */
 export const Router: React.FC = () => {
-    // const navigate = useNavigate();
-
-    // useEffect(() => {
-    //     if (!isAuthenticated) {
-    //         navigate('/');
-    //     }
-    // }, [isAuthenticated]);
-
-    // if (!isAuthenticated) {
-    //     return (
-    //         <div className={styles.container}>
-    //             <Spinner fixedHeight />
-    //         </Box>
-    //     );
-    // }
-
     return <RouterProvider router={browserRouter} />;
 };
